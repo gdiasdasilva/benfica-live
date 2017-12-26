d342d19bb7fafbd6249bc85c9a3909bc2bacc924
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2017-12-26 20:34:22 +0000

    Changed navbar to red

--- benfica-live/resources/views/layouts/app.blade.php
+++ benfica-live/resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
                 </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="/spots">Lista de Spots</a>
                     </li>
@@ -34,10 +34,6 @@
                         <a class="nav-link" href="/spots/new">Submeter um Spot</a>
                     </li>
                 </ul>
-                {{-- <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form> --}}
             </div>
         </nav>
         <div class="container">
