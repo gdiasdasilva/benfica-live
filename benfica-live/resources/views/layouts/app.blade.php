bd0fac60f70393cf95e25bb115ea5f814189875a
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2017-12-31 16:58:42 +0000

    Added GA

--- benfica-live/resources/views/layouts/app.blade.php
+++ benfica-live/resources/views/layouts/app.blade.php
@@ -28,5 +28,12 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111780420-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-111780420-1');
+    </script>
 </body>
 </html>
