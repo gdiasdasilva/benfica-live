362cbf7e91a3aef33985705d1699a9f4af35d302
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2018-01-06 20:44:12 +0000

    Only spots with image in homepage

--- benfica-live/app/Http/Controllers/HomeController.php
+++ benfica-live/app/Http/Controllers/HomeController.php
@@ -16,8 +16,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $recent_spots = Spot::orderBy('created_at', 'desc')->where('is_approved', true)->get()->random(3);
-
+        $recent_spots = Spot::orderBy('created_at', 'desc')->where('is_approved', true)->where('image', '!=', null)->get()->take(3);
         $spotsCount = Spot::all()->count();
         $countriesCount = Country::whereHas('spots', function($spot) {
             $spot->where('is_approved', true);
