086c1e77391a95a81020dd690467b121f31b763a
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2018-01-11 21:52:50 +0000

    Updated favicons for logo

--- benfica-live/resources/views/layouts/app.blade.php
+++ benfica-live/resources/views/layouts/app.blade.php
@@ -23,9 +23,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="icon" type="image/png" href="{{ asset('images/favicon-32x32.png') }}" sizes="32x32">
-    <link rel="icon" type="image/png" href="{{ asset('images/favicon-96x96.png') }}" sizes="96x96">
-    <link rel="icon" type="image/png" href="{{ asset('images/favicon-16x16.png') }}" sizes="16x16">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon-32x32.png') }}?v2" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon-96x96.png') }}?v2" sizes="96x96">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon-16x16.png') }}?v2" sizes="16x16">
     @yield('styles')
 </head>
 <body>
