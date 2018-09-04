744965d46a48e528c66939c14616bd6361fb79b7
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2018-09-04 18:46:17 +0100

    Refactors database tables and seeders

--- database/factories/SpotFactory.php
+++ database/factories/SpotFactory.php
@@ -3,15 +3,21 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Spot::class, function (Faker $faker) {
+    $fakeImage = $faker->image('public/storage/spots', 400, 400, 'sports', false);
+
     return [
         'name' => $faker->company,
         'slug' => str_slug($faker->company),
         'address' => $faker->address,
         'city' => $faker->city,
+        'image' => "spots/$fakeImage",
+        'thumbnail_image' => "spots/$fakeImage",
         'latitude' => (string) $faker->latitude,
         'longitude' => (string) $faker->longitude,
-        'country_id' => $faker->randomDigitNotNull,
-        'phone_number' => $faker->phoneNumber,
         'email' => $faker->email,
+        'phone_number' => $faker->phoneNumber,
+        'is_approved' => $faker->boolean(90),
+        'country_id' => \App\Country::inRandomOrder()->first()->id,
+        'website' => $faker->url,
     ];
 });
