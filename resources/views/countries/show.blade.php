2a246694c00124d175b739e05c536028db6f49de
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2018-09-16 01:42:00 +0100

    Adds country detail

--- resources/views/countries/show.blade.php
+++ resources/views/countries/show.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
-
 @section('pageTitle', $country->name_pt)
-
 @section('content')
 
-<div class="row country-detail">
-    <div class="col-md-6 mx-auto">
-        <h2>{{ $country->name_pt }} {{ $country->emoji }}</h2>
-        <div class="list-group">
-            @foreach ($country->spots as $spot)
-                <a href="{{ route('spots.show', ['countrySlug' => $country->slug_pt, 'spotSlug' => $spot->slug]) }}"
-                   class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                    {{ $spot->name }}
-                    <span class="badge badge-secondary">{{ $spot->city }}</span>
-                </a>
-            @endforeach
+<section class="section country-detail">
+    <div class="container">
+        <div class="columns">
+            <div class="column is-6 is-offset-3">
+                <h1 class="title">{{ $country->name_pt }} {{ $country->emoji }}</h1>
+                <h2 class="subtitle">Listagem de spots</h2>
+                <div class="panel">
+                    @foreach ($country->spots as $spot)
+                        <a class="panel-block" href="{{ route('spots.show', ['countrySlug' => $country->slug_pt, 'spotSlug' => $spot->slug]) }}">
+                            <span>{{ $spot->name }}</span>
+                            <span class="tag">{{ $spot->city }}</span>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <a class="btn btn-danger more-countries" href="{{ route('countries.index') }}">Ver outros países</a>
+        <div class="columns">
+            <div class="column is-2 is-offset-5">
+                <a class="button is-danger is-fullwidth" href="{{ route('countries.index') }}">Ver outros países</a>
+            </div>
+        </div>
     </div>
-</div>
+</section>
 
 @endsection
