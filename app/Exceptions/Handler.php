30e4f7886a9b04879c3f46bf1cb66e9c515a0c7e
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2018-11-21 22:26:25 +0000

    Only send exception e-mail if in production environment

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,7 +38,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        if ($this->shouldReport($exception)) {
+        if ($this->shouldReport($exception) && app()->environment() === 'production') {
             $this->sendEmail($exception);
         }
 
